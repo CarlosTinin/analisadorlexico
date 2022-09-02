8fb031cbf9b9507258c763b83cb34a26d5c87f49
Carlos Tinin <ctinin@brasilseg.com.br>
2022-09-01 21:54:12 -0300

    Analisador Léxico - Refatoração das funções

--- processor.py
+++ processor.py
@@ -14,10 +14,7 @@ class Processor:
 				self.process_character(char)
 		else:
 			if (self.state ==  13):
-				self.token_list.append(token.Token.generate_token(self.ids, 15, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 15, self.line_accumulator)
 
 	def process_character(self, char):
 		if (self.state == 0):
@@ -31,58 +28,28 @@ class Processor:
 				self.line_accumulator += char
 				self.state = 11
 			elif(char == ';'):
-				self.token_list.append(token.Token.generate_token(self.ids, 5, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 5, self.line_accumulator)
 			elif(char == ','):
-				self.token_list.append(token.Token.generate_token(self.ids, 6, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 6, self.line_accumulator)
 			elif(char == '('):
-				self.token_list.append(token.Token.generate_token(self.ids, 7, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 7, self.line_accumulator)
 			elif(char == ')'):
-				self.token_list.append(token.Token.generate_token(self.ids, 8, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 8, self.line_accumulator)
 			elif(char == '['):
-				self.token_list.append(token.Token.generate_token(self.ids, 9, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 9, self.line_accumulator)
 			elif(char == ']'):
-				self.token_list.append(token.Token.generate_token(self.ids, 10, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 10, self.line_accumulator)
 			elif(char == '{'):
-				self.token_list.append(token.Token.generate_token(self.ids, 11, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 11, self.line_accumulator)
 			elif(char == '}'):
-				self.token_list.append(token.Token.generate_token(self.ids, 12, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 12, self.line_accumulator)
 			elif(char == '.'):
-				self.token_list.append(token.Token.generate_token(self.ids, 13, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 13, self.line_accumulator)
 		elif (self.state == 1): # process q1 state
 			if(re.match(r'[A-Z]|[a-z]', char) or re.match(r'[1-9]', char) or (char == '_')):
 				self.line_accumulator += char
 			else: # build a identifier token and return to process character function
-				self.token_list.append(token.Token.generate_token(self.ids, 1, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 1, self.line_accumulator)
 				self.process_character(char)
 
 		elif (self.state == 11):
@@ -95,10 +62,7 @@ class Processor:
 
 		elif (self.state == 12): # this is a line block
 			if(char == "\n"):
-				self.token_list.append(token.Token.generate_token(self.ids, 3, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 3, self.line_accumulator)
 			else:
 				self.line_accumulator += char
 
@@ -110,25 +74,22 @@ class Processor:
 		elif(self.state == 14): # this is a block comment 
 			self.line_accumulator += char
 			if(char == '/'):
-				self.token_list.append(token.Token.generate_token(self.ids, 4, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 4, self.line_accumulator)
 			else:
 				self.state = 13
 
 		elif (self.state == 15): # this is a string
 			self.line_accumulator += char
 			if(char == '"'):
-				self.token_list.append(token.Token.generate_token(self.ids, 0, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 0, self.line_accumulator)
 			elif(char == '\n'):
-				self.token_list.append(token.Token.generate_token(self.ids, 14, self.line_accumulator))
-				self.ids += 1
-				self.state = 0
-				self.line_accumulator = ''
+				self.store_token_and_reset(self.ids, 14, self.line_accumulator)
 
 	def show_token_list(self):
 		return self.token_list;
+
+	def store_token_and_reset(self, token_id, token_type, content):
+		self.token_list.append(token.Token.generate_token(token_id, token_type, content))
+		self.ids += 1
+		self.state = 0
+		self.line_accumulator = ''
